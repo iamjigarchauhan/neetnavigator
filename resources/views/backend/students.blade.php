d546acbbbc146623024f142155806b41c3dac590
Kaushik <kaushik.moment@outlook.com>
2023-05-08 22:00:49 +0530

    Datatables

--- resources/views/backend/students.blade.php
+++ resources/views/backend/students.blade.php
@@ -61,14 +61,12 @@ h3 { font-size:20px}
 
 <script src="https://cdn.datatables.net/buttons/1.0.3/js/dataTables.buttons.min.js"></script>
 <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.0.3/css/buttons.dataTables.min.css">
-<script src="/vendor/datatables/buttons.server-side.js"></script>
-
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"> </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"> </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"> </script>
 <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"> </script>
-<script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.print.min.js"> </script>
+{{-- <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.print.min.js"> </script> --}}
+{{-- <script src="//cdn.datatables.net/buttons/2.3.6/css/buttons.semanticui.min.css"> </script> --}}
 {!! $dataTable->scripts() !!}
 
 {{-- <script>
