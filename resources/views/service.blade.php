8c2c75b29af3249cde076d0e4760ac73d03f8850
Mohammad Jafar <itzafar65@gmail.com>
2023-05-10 01:37:37 +0530

    done

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -1,34 +1,163 @@
-@extends('layouts.app')
-@section('content')
+@extends('layouts.app') @section('content')
 <section id="yl-popular-course" class="yl-popular-course-section">
+    <div class="container">
+        <div class="service_container">
+            <h2>Thousands of Problems Faced by Students</h2>
+            <div class="align">
+                <div class="box">
+                    <span>1</span>
+                    <h6>Thousands of students lost their seat due to lack of proper guidance and documentation.</h6>
+                </div>
+                <div class="box">
+                    <span>2</span>
+                    <h6>Many applications lack the proper knowledge of Mop-up and Stray Vacancy and Institute Level Rounds.</h6>
+                </div>
+                <div class="box">
+                    <span>3</span>
+                    <h6>Students fail to check website regu- larly, so they miss impotant dead- lines and lose their seat.</h6>
+                </div>
+                <div class="box">
+                    <span>4</span>
+                    <h6>Students fail to register because they think the process is only for MBBS/BDS and not for BAMS/BPTH.</h6>
+                </div>
+            </div>
+            <h2>And many many many more.......</h2>
+        </div>
+    </div>
+</section>
+<section id="blue_bg">
+    <div class="container">
+        <h2>ATMIA's Smart Admission Counseling Solutions</h2>
+        <div class="align_blue">
+            <div class="box_b">
+                <span><img src="{{ asset('assets/home/img/sign-way.png')}}" alt="" /></span>
+                <div class="text">
+                    <h6>Admission Guidance</h6>
+                    <ul>
+                        <li>Detailed stat-to-end guidance for admission</li>
+                        <li>Detailed information about colleges (Fee structure, location, ranking, facilities etc.)</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="box_b">
+                <span><img src="{{ asset('assets/home/img/7600118.png')}}" alt="" /></span>
+                <div class="text">
+                    <h6>Personalised Counselling</h6>
+                    <ul>
+                        <li>Personalised counselling by analysing your marks, reservation, fees etc.</li>
+                        <li>Round-wise counselling sessions to solve your queries of multiple processes</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="box_b">
+                <span><img src="{{ asset('assets/home/img/collage.png')}}" alt="" /></span>
+                <div class="text">
+                    <h6>College Preferences</h6>
+                    <ul>
+                        <li>Detailed Analysis of Colleges, seat matrix and previous cutofs.</li>
+                        <li>Personalised preference list for each round of Admission Process</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="box_b">
+                <span><img src="{{ asset('assets/home/img/menus.png')}}" alt="" /></span>
+                <div class="text">
+                    <h6>Round-wise College Cut-Offfs</h6>
+                    <ul>
+                        <li>College wise and Round wise previous year cutof's</li>
+                        <li>Cut-Offf will help you to understand where you can get admission</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="box_b">
+                <span><img src="{{ asset('assets/home/img/form.png')}}" alt="" /></span>
+                <div class="text">
+                    <h6>Admission Form Filling</h6>
+                    <ul>
+                        <li>Vigilant checking of documentation before stat of the process</li>
+                        <li>Filling up of Registration, Preference forms of multiple processes</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="box_b">
+                <span><img src="{{ asset('assets/home/img/sms.png')}}" alt="" /></span>
+                <div class="text">
+                    <h6>SMS/WhatsApp Alets</h6>
+                    <ul>
+                        <li>Prompt SMS alets of admission process Stray Round, so you never miss a deadline.</li>
+                        <li>All results, seat matrix, allotment lists, notices etc. updated instantly</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="box_b">
+                <span><img src="{{ asset('assets/home/img/2444194.png')}}" alt="" /></span>
+                <div class="text">
+                    <h6>Post Allotment Assistance</h6>
+                    <ul>
+                        <li>Assistance on inalising/Waiting for futher admission ater allotment</li>
+                        <li>Assistance on fees, location, DD details, hostel facility post inalisation.</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="box_b">
+                <span><img src="{{ asset('assets/home/img/2787549.png')}}" alt="" /></span>
+                <div class="text">
+                    <h6>Helpline & WhatsApp Suppot</h6>
+                    <ul>
+                        <li>Most impotant service to solve all your queries remotely and instantly</li>
+                        <li>Available on WhatsApp/Call at all hours for timely guidance</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <h2>With all these Services, we take care of your Entire Admission Process!</h2>
+    </div>
+</section>
+<section id="yl-popular-course" class="yl-popular-course-section">
+    <div class="container">
+      <div class="service_container two">
+        <h2>ATMIA NEET UG ALL INDIA & STATE<br/>COUNSELLINGS COLLEGE PREDICTOR</h2>
+        <p>Through ATMIA NEET COLLEGE PREDICTOR, Students can know their admission chances in Medical, BAMS, and BHMS colleges with NEET Rank and College fees.</p>
+        <ul>
+          <li>Students get their own personalized report with the finest Medical Colleges in India, which includes 15% of All INDIA quota seats of Government Colleges.</li>
+          <li>Can check Neet State-wise Cut-Offffs for more than 24 States of India.</li>
+          <li>ATMIA NEET College Predictor is an online tool or platform that aims to help students predict and identify the colleges they might be eligible for based on their NEET scores.</li>
+        </ul>
+        <p>ATMIA NEET College Predictor considers various factors such as the student's NEET score, category (General, OBC, SC/ST, etc.), state of residence, and sometimes other preferences like language, location, and reservation criteria. Using these inputs, the college predictor tool compares the student's score with the previous year's Cut-Off scores of different colleges and universities to generate a list of probable colleges where they might have a chance of getting admission.</p>
+        <p>It's important to note that the NEET College Predictor provides an estimate or prediction based on historical data and trends. The actual admission results may vary depending on several factors, including the number of applicants, the difficulty level of the exam, and changes in admission criteria, but ATMIA will help you get your MBBS seat in INDIAN Medical colleges by proper guidance form your form filling to the choice filling for the medical colleges.</p>
+      </div>
+    </div>
+</section>
+<section id="white_img">
   <div class="container">
+    <span><img src="{{ asset('assets/home/img/pc.png')}}" alt="" /></span>
   </div>
 </section>
 <section id="yl-newslatter" class="yl-newslatter-section-2 position-relative">
-  <div class="container">
-      <div class="yl-newslatter-content">
-        <div class="row">
-            <div class="col-md-6">
-              <div class="yl-newslatter-text-icon">
-                  <div class="yl-newslatter-icon float-left">
-                    <i class="flaticon-email"></i>
-                  </div>
-                  <div class="yl-newslatter-text yl-headline">
-                    <h3>Subscribe to a Newsletter!</h3>
-                    <span>You will get our update instantly! <img src="assets/img/like.png" alt=""></span>
-                  </div>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="yl-newslatter-form position-relative">
-                  <form action="#">
-                    <input type="email" placeholder="Enter your mail address...">
-                    <button>Submit <i class="fas fa-arrow-right"></i></button>
-                  </form>
-              </div>
+    <div class="container">
+        <div class="yl-newslatter-content">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="yl-newslatter-text-icon">
+                        <div class="yl-newslatter-icon float-left">
+                            <i class="flaticon-email"></i>
+                        </div>
+                        <div class="yl-newslatter-text yl-headline">
+                            <h3>Subscribe to a Newsletter!</h3>
+                            <span>You will get our update instantly! <img src="assets/img/like.png" alt="" /></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="yl-newslatter-form position-relative">
+                        <form action="#">
+                            <input type="email" placeholder="Enter your mail address..." />
+                            <button>Submit <i class="fas fa-arrow-right"></i></button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-      </div>
-  </div>
-</section> 
+    </div>
+</section>
 @endsection
