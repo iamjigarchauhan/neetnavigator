06bf5ed96e0fdfc012e7e522015dbb4439b8abe1
Kaushik <kaushik.moment@outlook.com>
2023-05-22 22:35:50 +0530

    TawkTo

--- resources/views/sections/_scripts.blade.php
+++ resources/views/sections/_scripts.blade.php
@@ -11,3 +11,14 @@
 <script src="{{ asset('assets/home/js/odometer.js') }}"></script>
 {{-- <script src="{{ asset('assets/home/js/jquery.nice-select.min.js') }}"></script> --}}
 <script src="{{ asset('assets/home/js/typer.js') }}"></script>
+<script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/646b43c5ad80445890ee5a9c/1h11g1fq0';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+</script>
